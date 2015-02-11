e68aea6b79542dba340d1eac70116a56f7df4572
Tim Hasselbeck <tjh778@gmail.com>
2015-02-10 23:21:08 -0500

    Adding AngularJS to Front-end

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 (function() 
 {
-	var app = angular.module('parkingDashboard', [ ]);
+	var app = angular.module('parkingDashboard', []);
 
 
-	app.controller('GarageController', function()
+	app.controller('ParkingController', function()
 	{
-		this.
+		this.garages = garages;
 	});
 	
 	
 	
+	var garages = [
+	    {name:"One Campus Martius", address:"1188 Farmer St, Detroit MI 48226", load:"low"},
+	    {name:"Premier", address:"1206 Woodward Ave, Detroit MI 48226", load:"high"},
+	    {name:"1001 Woodward", address:"1001 Woodward Ave, Detroit MI 48226", load:"low"},
+	    {name:"Book Cadillac", address:"150 Michigan Ave, Detroit MI 48226", load:"medium"},
+	    {name:"1 Detroit", address:"200 Larned St, Detroit MI 48226", load:"low"},
+	    {name:"2 Detroit", address:"160 E Congress St, Detroit MI 48226", load:"medium"},
+	    {name:"COBO Congress", address:"467 W Congress St, Detroit MI 48226", load:"full"},
+	    {name:"COBO Rooftop", address:"625 W Congress St, Detroit MI 48226", load:"medium"},
+	    {name:"COBO Washington", address:"475 Washington Ave, Detroit MI 48226", load:"low"},
+	    {name:"Detroit Opera House", address:"1426 Broadway St, Detroit MI 48226", load:"high"},
+	    {name:"Financial District", address:"730 Shelby St, Detroit MI 48226", load:"low"},
+	    {name:"AT&T Lot", address:"421 Bagley St, Detroit MI 48226", load:"low"},
+	    {name:"Brush St / Greektown", address:"1001 Brush St, Detroit MI 48226", load:"medium"},
+	    {name:"Fort St / Greektown", address:"419 E Fort St, Detroit MI 48226", load:"full"},
+	    {name:"First National Garage", address:"660 Woodward Ave, Detroit MI 48226", load:"low"},
+	    {name:"The Z", address:"1234 Library St, Detroit MI 48226", load:"low"},
+	    {name:"Howard Lot A", address:"1240 Abbot St, Detroit MI 48226", load:"high"},
+	    {name:"Howard Lot B", address:"1300 Abbot St, Detroit MI 48226", load:"medium"},
+	    {name:"Howard Lot C", address:"1100 Brooklyn St, Detroit MI 48226", load:"low"}
+	];
 	
 	
 	
@@ -17,9 +38,7 @@
 	
 	
 	
-	
-	
-	
+	/*
 	garageMarkers = [];
 	geocoder = new google.maps.Geocoder();
 
@@ -71,11 +90,11 @@
 
 
 		//Create Dropdown Entry
-		var newButton = $("<li id='garageButton" + garageNumber + "'></li>");
-		var newAnchor = $("<a href='#'>" + name + "</a>");
+		// var newButton = $("<li id='garageButton" + garageNumber + "'></li>");
+		// var newAnchor = $("<a href='#'>" + name + "</a>");
 		
-		newButton.html(newAnchor);
-		$('#dropdownTopBar').append(newButton);
+		// newButton.html(newAnchor);
+		// $('#dropdownTopBar').append(newButton);
 
 
 		//Make Dropdown Entry Do Something //TODO: no worky, use angularJS
@@ -108,18 +127,18 @@
 
 
 
-	var garagesArray = [];
+	// var garagesArray = [];
    
-	//Read Garages From JSON file
-	$.getJSON('garages.json', function(data) 
-	{    
-		for(var i=0; i<data.garage.length; i++)
-		{
-			var garagesArray[i] = data.garage[i];
-			AddGarage(garagesArray[i].name, garagesArray[i].address, garagesArray[i].load, i);
-		}
-	});
-	
+	// //Read Garages From JSON file
+	// $.getJSON('garages.json', function(data) 
+	// {    
+	// 	for(var i=0; i<data.garage.length; i++)
+	// 	{
+	// 		var garagesArray[i] = data.garage[i];
+	// 		AddGarage(garagesArray[i].name, garagesArray[i].address, garagesArray[i].load, i);
+	// 	}
+	// });
+	*/
 	
 	
 	
@@ -129,4 +148,4 @@
 	
 
 
-})
+})();
