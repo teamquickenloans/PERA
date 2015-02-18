3a1d95e8d7c06c2f2b4c014db83728b7773c3f05
Chelsea Bridson <bridsonc@msu.edu>
2015-02-18 00:14:01 -0500

    more merging

--- PERA/PERA/Migrations/Configuration.cs
+++ PERA/PERA/Migrations/Configuration.cs
@@ -64,7 +64,7 @@ namespace PERA.Migrations
             );
 
 
-            */
+       
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
