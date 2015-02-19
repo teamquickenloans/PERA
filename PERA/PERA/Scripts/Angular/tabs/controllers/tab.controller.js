c261daec017d19015c826c3eb3b3094596c0cc0a
windows virtual machine <bridsonc@msu.edu>
2015-02-19 16:30:54 -0500

    updated ER table

---
+++ PERA/PERA/Scripts/Angular/tabs/controllers/tab.controller.js
@@ -0,0 +1,37 @@
+﻿/**
+* Garages controller
+* @namespace thinkster.authentication.controllers
+*/
+(function () {
+    'use strict';
+
+    angular
+      .module('pera.tabs.controllers')
+      .controller('TabsController', TabController);
+
+
+    function TabController() {
+        this.tab = 1;
+
+        this.setTab = function (val) {
+            this.tab = val;
+        };
+
+        this.isSet = function (val) {
+            return this.tab === val;
+        };
+
+
+
+        //for nav bar side tabs ----------------------
+        this.sideTab = 0;
+
+        this.setSideTab = function (val) {
+            this.sideTab = val;
+        };
+
+        this.sideIsSet = function (val) {
+            return this.sideTab === val;
+        };
+    }
+})();
