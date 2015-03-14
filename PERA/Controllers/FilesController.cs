268790dd99dfd513b640a217e62cdc3a98558ce2
Chelsea Bridson <bridsonc@msu.edu>
2015-03-14 14:08:18 -0400

    further parser updates

--- PERA/Controllers/FilesController.cs
+++ PERA/Controllers/FilesController.cs
@@ -19,6 +19,7 @@ using Excel;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
+using PERA.HelperClasses;
 
 public struct columns
 {
@@ -36,7 +37,7 @@ namespace PERA.Controllers
 
         private PERAContext db = new PERAContext();
 
-        Dictionary<int, int> invoice_token_columns = new Dictionary<int, int>()
+        Dictionary<int, int> invoiceTokenColumns = new Dictionary<int, int>()
         {
             {5,3},
             {6,3},
@@ -51,7 +52,7 @@ namespace PERA.Controllers
             {16,3},
         };
 
-        Dictionary<int, int> invoice_name_columns = new Dictionary<int, int>()
+        Dictionary<int, int> invoiceNameColumns = new Dictionary<int, int>()
         {
 
         };
@@ -67,47 +68,25 @@ namespace PERA.Controllers
             var provider = GetMultipartProvider();
             var result = await Request.Content.ReadAsMultipartAsync(provider);
 
-            Invoice fileUploadObj = GetFormData(result);
-            
-            //Invoice newInvoice = JsonConvert.DeserializeObject<Invoice>(result.FormData);
-            Trace.WriteLine(fileUploadObj.InvoiceID);
+            Dictionary<string, string> invoice = GetFormData(result);
 
-
+            //string invoice = result.FormData.GetValues(0).FirstOrDefault();
+            //Invoice newInvoice = JsonConvert.DeserializeObject<Invoice>(fileUploadObj);
+           
+            Trace.WriteLine(invoice);
+            int i = 0;
             foreach(var file in result.FileData)
             {
-
                 // On upload, files are given a generic name like "BodyPart_26d6abe1-3ae1-416a-9429-b35f15e6e5d5"
                 // so this is how you can get the original file name
                 var originalFileName = GetDeserializedFileName(file);
-
-                //System.Diagnostics.Debug.WriteLine(originalFileName);
-           
                 var uploadedFileInfo = new FileInfo(file.LocalFileName);
-                System.Diagnostics.Debug.WriteLine(uploadedFileInfo);
 
-                FileStream stream = File.Open(file.LocalFileName, FileMode.Open, FileAccess.Read);
+                //System.Diagnostics.Debug.WriteLine(uploadedFileInfo);
 
-                // we use two separate packages to parse excel files, due to errors in ExcelDataReader OpenXmlReader
-
-                if (originalFileName.EndsWith(".xls"))
-                {
-                    System.Diagnostics.Debug.WriteLine("xls");
-                    // Reading from a binary Excel file ('97-2003 format; *.xls)
-                    XLSParser(stream, originalFileName);
-                }
-
-                else if (originalFileName.EndsWith(".xlsx"))
-                {
-                    //stream.Close();
-                    System.Diagnostics.Debug.WriteLine("xlsx");
-                    XLSParser(stream, originalFileName);
-                }
+                ExcelParser(file.LocalFileName, originalFileName, invoice, i);
+                i++;
             }
-
-
-            Trace.WriteLine("hello");
-            // Remove this line as well as GetFormData method if you're not
-            // sending any form data with your upload request
 
 
             // Through the request response you can return an object to the Angular controller
@@ -117,99 +96,32 @@ namespace PERA.Controllers
             return this.Request.CreateResponse(HttpStatusCode.OK, new { returnData });
         }
 
-        private void XLSXParser(string filename)
-        {
-
-            SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(filename, false);
-            WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
-            // Iterate through all WorksheetParts
-            foreach (WorksheetPart worksheetPart in workbookPart.WorksheetParts)
-            {
-                OpenXmlPartReader reader = new OpenXmlPartReader(worksheetPart);
-                string text;
-                string rowNum;
-                while (reader.Read())
-                {
-                   if (reader.ElementType == typeof(Cell))
-                    {
-                        Cell c = (Cell)reader.LoadCurrentElement();
-
-                        string cellValue;
-
-                        if (c.DataType != null && c.DataType == CellValues.SharedString)
-                        {
-                            SharedStringItem ssi = workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(int.Parse(c.CellValue.InnerText));
-
-                            cellValue = ssi.Text.Text;
-                        }
-                        else
-                        {
-                            cellValue = c.CellValue.InnerText;
-                        }
-
-                        Console.Out.Write("{0}: {1} ", c.CellReference, cellValue);
-                    }
-                          
-
-                    System.Diagnostics.Debug.WriteLine("xlsx for loop");
-                    Trace.WriteLine("row");
-                    do
-                    {
-
-                        if (reader.HasAttributes)
-                        {
-                            rowNum = reader.Attributes.First(a => a.LocalName == "r").Value;
-                            Trace.WriteLine("rowNum: " + rowNum);
-                        }
-
-                    } while (reader.ReadNextSibling()); // Skip to the next row
-
-                    break; // We just looped through all the rows so no 
-                    // need to continue reading the worksheet
-                }
-
-                if (reader.ElementType != typeof(Worksheet))
-                    reader.Skip();
-
-                reader.Close();
-            }        
-        }
-        
-
-        private void XLSParser(FileStream stream, string name)
+        private void ExcelParser(string path, string name, Dictionary<string, string> invoice, int index)
         {
             System.Diagnostics.Debug.WriteLine("begin excel parser");
-            IExcelDataReader excelReader = null;
-            
+            IExcelDataReader reader = null;
+            Trace.WriteLine(path);
+            var excelData = new ExcelData(path);
+            reader = excelData.getExcelReader(name);
 
 
-            if (name.EndsWith(".xls"))
-            {
-                excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
-            }
-
-            else if (name.EndsWith(".xlsx"))
-            {
-                //stream.Close();
-                System.Diagnostics.Debug.WriteLine("xlsx");
-                excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
-            }
-
-            if (excelReader == null)
+            /*if (reader == null)
             {
                 System.Diagnostics.Debug.WriteLine("excel reader null");
-            }
+            }*/
+
             // Create column names from first row
-            excelReader.IsFirstRowAsColumnNames = true;
+            reader.IsFirstRowAsColumnNames = true;
 
             // The result of each spreadsheet will be created in the result.Tables
-            System.Diagnostics.Debug.WriteLine("creating dataset..");
-            DataSet result = excelReader.AsDataSet();
-            System.Diagnostics.Debug.WriteLine(result);
+            DataSet result = reader.AsDataSet();
 
             List<ParkerReportTeamMember> teamMembers = new List<ParkerReportTeamMember>();
-            var table = result.Tables[0];
+            
             System.Diagnostics.Debug.WriteLine("begin for loop");
+
+            foreach (DataTable table in result.Tables)
+            { 
                 foreach (DataRow row in table.Rows)
                 {
                     // if this row is the headings, skip this row
@@ -219,13 +131,13 @@ namespace PERA.Controllers
                         System.Diagnostics.Debug.WriteLine("null");
                         continue;
                     }
-                    //System.Diagnostics.Debug.WriteLine("b4");
+
                     if(row[0] is String)
                     {
                         System.Diagnostics.Debug.WriteLine(row[0]);
                         continue;
                     }
-                    System.Diagnostics.Debug.WriteLine("teamMember");
+                   
                     ParkerReportTeamMember teamMember = new ParkerReportTeamMember();
 
                     string fullName = (string)row[2];
@@ -246,14 +158,10 @@ namespace PERA.Controllers
                     //string cardNumber = (string)row[3];
                     double cardNumberD = 0;
                     int cardNumber = 0;
-                    for(int i = 3; i < 8; i++)
-                    {
-                        if(row[i] is System.DBNull)
-                            continue;
-                        //System.Diagnostics.Debug.WriteLine(row[i].GetType());
-                        cardNumberD = (System.Double)row[i];
-                        cardNumber = Convert.ToInt32(cardNumberD);
-                    }
+                    
+                    cardNumberD = (System.Double)row[1];
+                    cardNumber = Convert.ToInt32(cardNumberD);
+                    
 
 
                     teamMember.FirstName = firstName;
@@ -272,8 +180,7 @@ namespace PERA.Controllers
                     }*/
                                        
                 } // end for columns
-            //}  end for tables
-
+            }  // end for tables
         }
 
 
@@ -290,7 +197,7 @@ namespace PERA.Controllers
         }
 
         // Extracts Request FormatData as a strongly typed model
-        private Invoice GetFormData(MultipartFormDataStreamProvider result)
+        private Dictionary<string,string> GetFormData(MultipartFormDataStreamProvider result)
         {
             if (result.FormData.HasKeys())
             {
@@ -299,7 +206,7 @@ namespace PERA.Controllers
                 if (!String.IsNullOrEmpty(unescapedFormData))
                 {
                     System.Diagnostics.Debug.WriteLine("inside if form data");
-                    return JsonConvert.DeserializeObject<Invoice>(result.FormData.GetValues(0).FirstOrDefault());
+                    return JsonConvert.DeserializeObject<Dictionary<string, string>>(result.FormData.GetValues(0).FirstOrDefault());
                 }
                     
             }
@@ -317,5 +224,66 @@ namespace PERA.Controllers
         {
             return fileData.Headers.ContentDisposition.FileName;
         }
+
+        /*  private void XLSXParser(string filename)
+  {
+
+      SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(filename, false);
+      WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+      // Iterate through all WorksheetParts
+      foreach (WorksheetPart worksheetPart in workbookPart.WorksheetParts)
+      {
+          OpenXmlPartReader reader = new OpenXmlPartReader(worksheetPart);
+          string text;
+          string rowNum;
+          while (reader.Read())
+          {
+             if (reader.ElementType == typeof(Cell))
+              {
+                  Cell c = (Cell)reader.LoadCurrentElement();
+
+                  string cellValue;
+
+                  if (c.DataType != null && c.DataType == CellValues.SharedString)
+                  {
+                      SharedStringItem ssi = workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(int.Parse(c.CellValue.InnerText));
+
+                      cellValue = ssi.Text.Text;
+                  }
+                  else
+                  {
+                      cellValue = c.CellValue.InnerText;
+                  }
+
+                  Console.Out.Write("{0}: {1} ", c.CellReference, cellValue);
+              }
+                          
+
+              System.Diagnostics.Debug.WriteLine("xlsx for loop");
+              Trace.WriteLine("row");
+              do
+              {
+
+                  if (reader.HasAttributes)
+                  {
+                      rowNum = reader.Attributes.First(a => a.LocalName == "r").Value;
+                      Trace.WriteLine("rowNum: " + rowNum);
+                  }
+
+              } while (reader.ReadNextSibling()); // Skip to the next row
+
+              break; // We just looped through all the rows so no 
+              // need to continue reading the worksheet
+          }
+
+          if (reader.ElementType != typeof(Worksheet))
+              reader.Skip();
+
+          reader.Close();
+      }        
+  }
+        
+  */
     }
 }
+
---
+++ PERA/HelperClasses/ExcelData.cs
@@ -0,0 +1,63 @@
+﻿using Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace PERA.HelperClasses
+{
+    public class ExcelData
+    {
+        string _path;
+        IExcelDataReader _reader;
+
+        public ExcelData(string path)
+        {
+            _path = path;
+            _reader = null;
+        }
+
+        public IEnumerable<string> getWorksheetNames()
+        {
+            var reader = _reader;
+            var workbook = reader.AsDataSet();
+            var sheets = from DataTable sheet in workbook.Tables select sheet.TableName;
+            return sheets;
+        }
+
+        public IEnumerable<DataRow> getData(string sheet, bool firstRowIsColumnNames = true)
+        {
+            var reader = _reader;
+            reader.IsFirstRowAsColumnNames = firstRowIsColumnNames;
+            var workSheet = reader.AsDataSet().Tables[sheet];
+            var rows = from DataRow a in workSheet.Rows select a;
+            return rows;
+        }
+
+        public IExcelDataReader getExcelReader(string name)
+        {
+            // ExcelDataReader works with the binary Excel file, so it needs a FileStream
+            // to get started. This is how we avoid dependencies on ACE or Interop:
+            FileStream stream = File.Open(_path, FileMode.Open, FileAccess.Read);
+
+            // We return the interface, so that
+            try
+            {
+                if (name.EndsWith(".xls"))
+                {
+                    _reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                }
+                if (name.EndsWith(".xlsx"))
+                {
+                    _reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                }
+                return _reader;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
