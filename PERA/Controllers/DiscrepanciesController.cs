a18299dd473c91592048078c86c9384bb111f4ef
Chelsea Bridson <bridsonc@msu.edu>
2015-04-11 22:35:43 -0400

    played with layout and removed duplicates from sys gal

--- PERA/Controllers/DiscrepanciesController.cs
+++ PERA/Controllers/DiscrepanciesController.cs
@@ -15,7 +15,7 @@ namespace PERA.Controllers
 {
     public class DiscrepanciesController : Controller
     {
-        private PERAContext db = new PERAContext();
+        PERAContext db = new PERAContext();
 
         protected override void Dispose(bool disposing)
         {
@@ -45,6 +45,7 @@ namespace PERA.Controllers
 
             Trace.WriteLine(report.GarageID);
 
+
             // invoice report for this garage only
             var InvoiceReportQ = db.InvoiceActiveParkerReports.Where(
                 x => x.MonthYear.Month == report.MonthYear.Month
@@ -73,75 +74,139 @@ namespace PERA.Controllers
                 Trace.WriteLine("invoiceReport " + invoiceReport.GarageID);
             }
 
-            var duplicates = IdentifyDuplicates(InvoiceReport, QLReport, InvoiceReports);
+
+            //RemoveDuplicatesFromSystemGalaxy();
+            var duplicates = IdentifyDiscrepancies(InvoiceReport, QLReport, InvoiceReports);
 
             return Json(duplicates, JsonRequestBehavior.AllowGet);
         }
 
-        public List<List<Discrepancy>> IdentifyDuplicates(InvoiceActiveParkerReport InvoiceReport, QLActiveParkerReport QLReport,
+        //
+        public void RemoveDuplicatesFromSystemGalaxy(QLActiveParkerReport QLReport)
+        {
+            List<QLTeamMember> Duplicates = new List<QLTeamMember>(); 
+            Dictionary <string, string> Matches = new Dictionary <string, string>();
+
+            foreach (QLTeamMember qlTM in QLReport.TeamMembers)
+            {
+                //var qlTM = QLReport.TeamMembers[i];
+                // Get the most recent badge scan for this TM
+                PERAContext db = new PERAContext();
+                var BadgeScan = db.BadgeScans.Where(
+                     x => x.FirstName == qlTM.FirstName
+                       && x.LastName == qlTM.LastName)
+                .OrderByDescending(x => x.ScanDateTime)
+                    .FirstOrDefault();
+
+                string qlName = qlTM.FirstName + qlTM.LastName;
+
+                if (Matches.ContainsKey(qlName)) // it's a duplicate
+                    db.ParkerReportTeamMembers.Remove(qlTM);
+                //    continue;
+                else
+                {    //check if it's valid
+                    if (qlTM.BadgeID == BadgeScan.BadgeID)
+                        Matches[qlName] = qlName;
+                    else // it's invalid so remove it later
+                        db.ParkerReportTeamMembers.Remove(qlTM);
+                    //    Duplicates.Add(qlTM);
+                }
+            }
+            //for (int i = Duplicates.Count - 1; i > -1; i--)
+            //{
+            //    PERAContext db = new PERAContext();
+            //    db.ParkerReportTeamMembers.Remove(Duplicates[i]);
+            //}
+        }
+
+
+
+        
+        // Identifies discrepancies
+        
+        public List<List<Discrepancy>> IdentifyDiscrepancies(InvoiceActiveParkerReport InvoiceReport, QLActiveParkerReport QLReport,
+        
             List<InvoiceActiveParkerReport> InvoiceReports)
         {
+
+            
             List<List<Discrepancy>> issues = new List<List<Discrepancy>>();
+
             //Dictionary<string, string> Matches = new Dictionary<string, string>();
+            
             List<Discrepancy> Duplicates = new List<Discrepancy>();
 
+
+            
             //team members in the QL report that were not in the invoice
             List<Discrepancy> Missing = new List<Discrepancy>();
 
-            //team members in the invoice that were not in the QL report
+            //team members in the invoice that we re not in the QL report
             ICollection<ParkerReportTeamMember> Extra = InvoiceReport.TeamMembers;
             List<Discrepancy> Extras = new List<Discrepancy>();
 
-            List<Discrepancy> Discrepancies = new List<Discrepancy>();
+            //List<Discrepancy> Discrepancies = new List<Discrepancy>();
 
             Trace.WriteLine(InvoiceReport.TeamMembers.First());
 
             foreach (QLTeamMember qlTM in QLReport.TeamMembers) //for each TM in the qlReport
             {
-                //First find duplicates within this garage
+                PERAContext db = new PERAContext();
+                //Trace.WriteLine(qlTM.FirstName + " " + qlTM.LastName + QLReport.ID);
 
-                Trace.WriteLine(qlTM.FirstName + " " + qlTM.LastName + QLReport.ID);
+                // Grab the Correct Matching Invoice Team Member find duplicates within this garage
+                //  with the correct badge ID
 
-                    var Matches = db.ParkerReportTeamMembers.Where(             //grab the matching Invoice TM
-                        x => x.InvoiceActiveParkerReportID == InvoiceReport.ID
-                          && x.FirstName == qlTM.FirstName
-                          && x.LastName == qlTM.LastName
-                          && x.BadgeID == qlTM.BadgeID);
+                //TODO: Use the "last used" Badge Scan activity as the correct badgeID
+                var Matches = db.ParkerReportTeamMembers.Where(             //grab the matching Invoice TM
+                    x => x.InvoiceActiveParkerReportID == InvoiceReport.ID
+                      && x.FirstName == qlTM.FirstName
+                      && x.LastName == qlTM.LastName
+                      && x.BadgeID == qlTM.BadgeID);
 
-                    var MatchesList = Matches.ToList();
-                    ParkerReportTeamMember InvoiceTeamMember;
-                    switch (MatchesList.Count)
-                    {
-                        case 0:
-                            //Correct information for TM missing from Invoice
-                            Discrepancy discrepancy = db.Discrepancies.Create();
-                            discrepancy.FirstName = qlTM.FirstName;
-                            discrepancy.LastName = qlTM.LastName;
-                            discrepancy.TokenID = qlTM.BadgeID;
-                            discrepancy.Action = "Add";
-                            Missing.Add(discrepancy);
-                            break;
-                        case 1:
-                            InvoiceTeamMember = MatchesList.First();
+                var MatchesList = Matches.ToList();
+                ParkerReportTeamMember InvoiceTeamMember;
+
+                switch (MatchesList.Count)
+                {
+                    case 0:
+                        // Correct information for TM missing from Invoice, so we need to add it
+                        Discrepancy discrepancy = db.Discrepancies.Create();
+                        discrepancy.FirstName = qlTM.FirstName;
+                        discrepancy.LastName = qlTM.LastName;
+                        discrepancy.TokenID = qlTM.BadgeID;
+                        discrepancy.Action = "Add";
+                        Missing.Add(discrepancy);
+                        break;
+                    case 1:
+                        // if there's only one, grab it
+                        InvoiceTeamMember = MatchesList.First();
+                        Extra.Remove(InvoiceTeamMember);
+                        break;
+                    default:
+                        // more than one, grab the first
+                        InvoiceTeamMember = MatchesList.First();
+
+                        // and remove it from the matches list
+                        MatchesList.Remove(InvoiceTeamMember);
+
+                        // also remove it from the "extras"
+                        Extra.Remove(InvoiceTeamMember);
+
+                        foreach (var match in MatchesList)
+                        {
+                            Discrepancy disc = db.Discrepancies.Create();
+                            disc.FirstName = match.FirstName;
+                            disc.LastName = match.LastName;
+                            disc.TokenID = match.BadgeID;
+                            disc.Action = "Remove";
+                            Duplicates.Add(disc);
                             Extra.Remove(InvoiceTeamMember);
-                            break;
-                        default:
-                            InvoiceTeamMember = MatchesList.First();
-                            Extra.Remove(InvoiceTeamMember);
-                            MatchesList.Remove(InvoiceTeamMember);
-                            foreach(var match in MatchesList){
-                                Discrepancy disc = db.Discrepancies.Create();
-                                disc.FirstName = match.FirstName;
-                                disc.LastName = match.LastName;
-                                disc.TokenID = match.BadgeID;
-                                disc.Action = "Remove";
-                                Duplicates.Add(disc);
-                                Extra.Remove(InvoiceTeamMember);
-                            }
-                            break;
+                        }
+                        break;
                 }
 
-                Trace.WriteLine(qlTM.FirstName + " " + qlTM.LastName + QLReport.ID);
+                //Trace.WriteLine(qlTM.FirstName + " " + qlTM.LastName + " " + QLReport.ID);
                 var InvoiceTeamMembers = new List<ParkerReportTeamMember>();
                 /*InvoiceTeamMembers = db.ParkerReportTeamMembers.Where(             //grab the matching Invoice TM
                         x => x.InvoiceActiveParkerReportID == InvoiceReport.ID
@@ -150,6 +215,9 @@ namespace PERA.Controllers
                       .ToList();*/
 
                 //List<int> tempIdList = MatchesList.Select(q => q.ParkerReportTeamMemberID ).ToList();
+
+                // find team members with incorrect badge ID's
+
                 InvoiceTeamMembers = db.ParkerReportTeamMembers.Where(
                     //q => !tempIdList.Contains(q.ParkerReportTeamMemberID)
                       q => q.InvoiceActiveParkerReportID == InvoiceReport.ID
@@ -158,16 +226,17 @@ namespace PERA.Controllers
                         && q.BadgeID != qlTM.BadgeID)
                         .ToList();
 
-                if(InvoiceTeamMembers.Count() > 0)
+                if (InvoiceTeamMembers.Count() > 0)
                 {
-                    foreach(var itm in InvoiceTeamMembers){
+                    foreach (var itm in InvoiceTeamMembers)
+                    {
                         Discrepancy discrepancy = db.Discrepancies.Create();
                         discrepancy.FirstName = itm.FirstName;
                         discrepancy.LastName = itm.LastName;
                         discrepancy.TokenID = itm.BadgeID;
                         discrepancy.Action = "Remove";
                         Duplicates.Add(discrepancy);
-                        }
+                    }
                 }
                 // Next find duplicates within the garage network
                 /*foreach (InvoiceActiveParkerReport invoiceReport in InvoiceReports)
@@ -194,7 +263,8 @@ namespace PERA.Controllers
 
                 }*/
             } //end loop of qlTM
-            foreach (var duplicate in Duplicates) {
+            foreach (var duplicate in Duplicates)
+            {
                 Trace.WriteLine("Duplicate: " + duplicate.FirstName + " " + duplicate.LastName + " " + duplicate.TokenID + " " + duplicate.Action);
 
             }
@@ -202,6 +272,7 @@ namespace PERA.Controllers
             {
                 Trace.WriteLine("Missing: " + missing.FirstName + " " + missing.LastName + " " + missing.TokenID);
             }
+            // create a discrepacy for each person left in the "extra" list
             foreach (var extra in Extra)
             {
                 Discrepancy disc = db.Discrepancies.Create();
@@ -215,8 +286,8 @@ namespace PERA.Controllers
             issues.Add(Duplicates);
             issues.Add(Extras);
             issues.Add(Missing);
-            issues.Add(Discrepancies);
+            //issues.Add(Discrepancies);
             return issues;
-        } //  Identify Duplicates
+        }
     }
 }
