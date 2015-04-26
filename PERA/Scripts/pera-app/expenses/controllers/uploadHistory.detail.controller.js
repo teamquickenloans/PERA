94741a11d85f7a27bb611c1ab496f58a8f770c8b
Chelsea Bridson <bridsonc@msu.edu>
2015-04-26 15:32:17 -0400

    more updates

--- PERA/Scripts/pera-app/expenses/controllers/uploadHistory.detail.controller.js
+++ PERA/Scripts/pera-app/expenses/controllers/uploadHistory.detail.controller.js
@@ -8,20 +8,23 @@
     angular
       .module('pera.expenses.controllers')
       .controller('UploadHistoryDetailController', UploadHistoryDetailController);
-    UploadHistoryDetailController.$inject = ['$scope', 'Snackbar', '$stateParams', 'TeamMembers']
+    UploadHistoryDetailController.$inject = ['$scope', 'Snackbar', '$stateParams', 'TeamMembers', 'InvoiceAPRs', 'QLAPRs']
 
-    function UploadHistoryDetailController($scope, Snackbar, $stateParams, TeamMembers) {
+    function UploadHistoryDetailController($scope, Snackbar, $stateParams, TeamMembers, InvoiceAPRs, QLAPRs) {
 
         console.log($stateParams.reportId);
+        $scope.report;
 
-        if ($stateParams.typeId == 1)
-            getTeamMembers();
 
-        else if ($stateParams.typeId == 2)
-            TeamMembers.qlreport($stateParams.reportId).then(success, error);
 
-        function getTeamMembers() {
+        if ($stateParams.typeId == 1) {
             TeamMembers.invoice($stateParams.reportId).then(success, error);
+            InvoiceAPRs.get($stateParams.reportId).then(reportSuccess, reportError);
+
+        }
+        else if ($stateParams.typeId == 2) {
+            TeamMembers.qlreport($stateParams.reportId).then(success, error);
+            QLAPRs.get($stateParams.reportId).then(reportSuccess, reportError);
         }
 
         function success(data) {
@@ -30,6 +33,18 @@
 
         function error(data) {
             Snackbar.error("Team Members could not be retrieved");
+        }
+
+        function invoiceSuccess(data) {
+            $scope.report = data.data;
+        }
+
+        function reportSuccess(data) {
+            $scope.report = data.data;
+        }
+
+        function reportError(data) {
+            Snackbar.error("failed to retrieve report");
         }
     }
 })()
